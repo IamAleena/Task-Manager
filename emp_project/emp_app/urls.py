94571399867e581510d32bf0daa97e5acd2dcb16
IamAleena <aleenasusan0001@gmail.com>
2023-07-18 14:48:00 +0530

    change

--- emp_project/emp_app/urls.py
+++ emp_project/emp_app/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('api/jobs/<int:pk>/', JobView.as_view(), name='job-detail'),
     path('api/assigned-jobs/', AssignedJobsView.as_view(), name='assigned-jobs'),
     path('api/jobs/<int:pk>/status/', JobStatusView.as_view(), name='job-status'),
-
 ]
